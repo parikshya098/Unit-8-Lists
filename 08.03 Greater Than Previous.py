54aec410951a1c4041fd86c98bd34803bfe78ca6
parikshya098 <pbhandari1@ualr.edu>
2022-03-15 04:56:54 +0000

    Unit 8

---
+++ 08.03 Greater Than Previous.py
@@ -0,0 +1,9 @@
+my_list =  input("Enter Values Separated by Spaces: ").split()
+my_list=[int(i) for i in my_list]
+
+temp = my_list[0]
+n = len(my_list) 
+for i in range(1,n):
+	if my_list[i]>temp:
+		print(my_list[i])
+	temp = my_list[i]
