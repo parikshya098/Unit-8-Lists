68ed25c03d76ac298490a7b863f83116925330c6
parikshya098 <pbhandari1@ualr.edu>
2022-03-16 02:53:34 +0000

    Unit 8

---
+++ 08.04 Neighbors Of The Same Sign.py
@@ -0,0 +1,8 @@
+string_of_nums = input('Enter Values Separated by Spaces: ')
+y = [int(q) for q in string_of_nums.split()]
+
+for i in range(len(y)-1):
+    if y[i] >= 0 and y[i+1] >=0: 
+        print(y[i] ,y[i+1])   
+    elif y[i] < 0 and y[i+1] <0: 
+        print(y[i] ,y[i+1])   
